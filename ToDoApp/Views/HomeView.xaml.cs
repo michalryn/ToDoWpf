dbcd353cc97b0f1b797385a61ee7fe1092cb00db
Jakub Feliński <80295@student.pb.edu.pl>
2023-06-22 16:47:49 +0200

    Sorting via Title and Priority

--- ToDoApp/Views/HomeView.xaml.cs
+++ ToDoApp/Views/HomeView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ToDoApp.Models;
 
 namespace ToDoApp.Views
 {
@@ -23,6 +24,58 @@ namespace ToDoApp.Views
         public HomeView()
         {
             InitializeComponent();
+
+            FilterBy.ItemsSource = new string[] { "Title", "Priority"};
+
+        }
+
+        public Predicate<object> GetFilter()
+        {
+            switch (FilterBy.SelectedItem as string) 
+            {
+                case "Title":
+                    return TitleFilter;
+
+                case "Priority":
+                    return PriorityFilter;
+
+            }
+
+            return TitleFilter;
+        }
+
+        private bool TitleFilter(object obj)
+        {
+            var Filterobj = obj as MainTask;
+
+            return Filterobj.Title.Contains(FilterTextBox.Text, StringComparison.OrdinalIgnoreCase);
+
+        }
+
+        private bool PriorityFilter(object obj)
+        {
+            var Filterobj = obj as MainTask;
+
+            return Filterobj.PriorityLevel.Contains(FilterTextBox.Text, StringComparison.OrdinalIgnoreCase);
+
+        }
+
+
+        private void FilterBy_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(FilterTextBox.Text == null)
+            {
+                TasksList.Items.Filter = null;
+            }
+            else
+            {
+                TasksList.Items.Filter = GetFilter();
+            }
+        }
+
+        private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TasksList.Items.Filter = GetFilter();
         }
     }
 }
